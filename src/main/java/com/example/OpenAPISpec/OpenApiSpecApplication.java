0160a90a2c1558827132425ddd48de451572e6ca
vinayak.warad <vinayr.warad@gmail.com>
2025-07-04 16:38:07 +0530

    OpenAPI implementation sample with correction in openAPI file 04/07/2025

--- src/main/java/com/example/OpenAPISpec/OpenApiSpecApplication.java
+++ src/main/java/com/example/OpenAPISpec/OpenApiSpecApplication.java
@@ -4,7 +4,6 @@ import com.example.OpenAPISpec.entity.Student;
 import com.example.OpenAPISpec.repository.StudentRepository;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
